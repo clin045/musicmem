489788603952c79fa4500cfcc8a2a3e928888fe3
Christopher Lin <clin5@bidmc.harvard.edu>
2019-09-07 20:54:38 -0400

    refactored into cloze method, returns list of paths

--- musicmem.py
+++ musicmem.py
@@ -1,6 +1,21 @@
 import pydub
 import os
 
+def make_cloze(start_point, end_point, prompt_len, ans_len, music_name, out_path):
+    clips = []
+    for i in range(start_point, end_point, prompt_len):
+            prompt_clip = music[i:i+prompt_len]
+            prompt_clip.fade_in(20).fade_out(20)
+            prompt_clip_path = (out_path+"/"+music_name+"_"+str(i)+"_prompt.mp3")
+            prompt_clip.export(prompt_clip_path,format='mp3')
+            ans_clip = music[i:i+prompt_len+ans_len]
+            ans_clip.fade_in(20).fade_out(20)
+            ans_clip_path = (out_path+"/"+music_name+"_"+str(i)+"_ans.mp3")
+            ans_clip.export(ans_clip_path, format='mp3')
+
+            clips.append((prompt_clip_path, ans_clip_path))
+    return(clips)
+
 
 if __name__ == '__main__':
     music_file = './test_data/in_heaven.mp3'
@@ -12,13 +27,8 @@ if __name__ == '__main__':
     prompt_len = 5 * 1000
     ans_len = 5 * 1000
     end_len = 5 * 1000
-    for i in range(start_point, end_point, prompt_len):
-        prompt_clip = music[i:i+prompt_len]
-        prompt_clip.fade_in(20).fade_out(20)
-        prompt_clip.export('./audio_data/' + music_name+'_' + str(i)+'_prompt.mp3',format='mp3')
-        ans_clip = music[i:i+prompt_len+ans_len]
-        prompt_clip.fade_in(20).fade_out(20)
-        ans_clip.export('./audio_data/'+music_name+'_' + str(i) +'_ans.mp3', format='mp3')
+    clip_list = make_cloze(start_point, end_point, prompt_len, ans_len, music_name, './audio_data')
 
+    print(clip_list)
 
 
